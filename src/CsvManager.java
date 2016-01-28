9902c42a3dcf7ca30f044afcdd1133e5fe4b88c7
thiagoliveira <thiagoliveira08@gmail.com>
2016-01-28 16:17:49 +0100

    Add CsvManager

--- src/CsvManager.java
+++ src/CsvManager.java
@@ -1,38 +1,75 @@
-import com.csvreader.*;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import au.com.bytecode.opencsv.*;
 
 public class CsvManager {
+	DateFormat dateFormat = new SimpleDateFormat("ddMMyyyy-HHmmss"); 
+	Date date = new Date();
+	private String outputFile = dateFormat.format(date)+"-log.csv";
 	
-	private String outputFile = "data.csv";
+	private CSVWriter csvOutput;
 	
-	private CsvWriter csvOutput;
-	
+	private ArrayList<String[]> dataList;
+
 	public CsvManager() throws IOException{
 		
-		csvOutput = new CsvWriter(outputFile);
-		boolean alreadyExists = new File(outputFile).exists();
+		dataList = new ArrayList<String[]>();
 		
+		csvOutput = new CSVWriter(new FileWriter(outputFile),'\t');
+		boolean alreadyExists = new File(outputFile).exists();
+		System.out.println(alreadyExists);
+				
 		if (!alreadyExists)
 		{
-			csvOutput.write("id");
-			csvOutput.write("name");
-			csvOutput.endRecord();
+			csvOutput.writeNext("id");
+			csvOutput.writeNext("name");
+			System.out.println("aqui");
 		}
 	}
 	
-	public void registerData(int id, String name){
-		try {
-			csvOutput.write(""+id);
-			csvOutput.write(name);
-			csvOutput.endRecord();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void registerData(String[] data) throws IOException{
+		csvOutput = new CSVWriter(new FileWriter(outputFile),'\t');
+		dataList.add(data);
+	    csvOutput.writeAll(dataList);      
+	    csvOutput.close();	    
+	}
+	
+	public void registerData(String data, int row, int column) throws IOException{
+		csvOutput = new CSVWriter(new FileWriter(outputFile),';');
+		dataList.get(row)[column] = data;
+	    csvOutput.writeAll(dataList);      
+	    csvOutput.close();	    
 	}
 	
 	public void readData(){
 		
 	}
+	
+	private void addData(String[] data){
+		this.dataList.add(data);
+	}
+	
+	public void setData(String data, int row, int column){
+		this.dataList.get(row)[column] = data;
+	}
+	
+	public String[] getData(int row){
+		return this.dataList.get(row);
+	}
+	
+	public String getData(int row, int column){
+		return this.dataList.get(row)[column];
+	}
+	
+	public ArrayList<String[]> getDataList(){
+		return dataList;
+	}
+	
+	
+
 }
